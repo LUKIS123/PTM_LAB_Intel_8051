7a122d0c7ef772df6e7038d835fdef8c019cf5d6
Czerworopucha <59446206+Czerworopucha@users.noreply.github.com>
2023-05-23 10:27:49 +0200

    lab6 template

---
+++ LAB6/lab6.asm
@@ -0,0 +1,58 @@
+;---------------------------------------------------------------------
+P5		EQU	0F8h		; adres P5 w obszarze SFR
+P7		EQU	0DBh		; adres P7 w obszarze SFR
+;---------------------------------------------------------------------
+ROWS		EQU	P5		; wiersze na P5.7-4
+COLS		EQU	P7		; kolumny na P7.3-0
+;---------------------------------------------------------------------
+LEDS		EQU	P1		; diody LED na P1 (0=ON)
+;---------------------------------------------------------------------
+
+ORG 0
+
+main_loop:
+	lcall	kbd_read
+	lcall	kbd_display
+	sjmp	main_loop
+
+;---------------------------------------------------------------------
+; Uaktywnienie wybranego wiersza klawiatury
+;
+; Wejscie: A - numer wiersza (0 .. 3)
+;---------------------------------------------------------------------
+kbd_select_row:
+
+	ret
+
+;---------------------------------------------------------------------
+; Odczyt wybranego wiersza klawiatury
+;
+; Wejscie: A  - numer wiersza (0 .. 3)
+; Wyjscie: CY - stan wiersza (0 - brak klawisza, 1 - wcisniety klawisz)
+;	   A  - kod klawisza (0 .. 3)
+;---------------------------------------------------------------------
+kbd_read_row:
+
+	ret
+
+;---------------------------------------------------------------------
+; Odczyt calej klawiatury
+;
+; Wyjscie: CY - stan klawiatury (0 - brak klawisza, 1 - wcisniety klawisz)
+; 	   A - kod klawisza (0 .. 15)
+;---------------------------------------------------------------------
+kbd_read:
+
+	ret
+
+;---------------------------------------------------------------------
+; Wyswietlenie stanu klawiatury
+;
+; Wejscie: CY - stanu klawiatury (0 - brak klawisza, 1 - wcisniety klawisz)
+; 	   A  - kod klawisza (0 .. 15)
+;---------------------------------------------------------------------
+kbd_display:
+
+	ret
+
+END
